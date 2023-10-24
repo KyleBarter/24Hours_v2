6facb3d96a173d190c32c0449a578c6f761396bb
KyleBarter <kylebarter4@gmail.com>
2023-10-24 16:01:17 +0100

    model commit for mac pull

---
+++ models/activity.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//? journal schema
+const journalSchema = new Schema({
+    journalEntry: {
+        type: String,
+        required: true
+    }, 
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        require: true
+    },
+},  {
+    timestamps: true
+    });
+
+
+//?activity schema
+const activitySchema = new Schema ({
+    activity: {
+        type: String,
+        required: true
+    },
+    activityType: {
+        type: String,
+        enum: ['Cooking', 'Exercise', 'Gardening', 'Hobby', 'Reading', 'Religious practice', 'Hygiene', 'Sleeping', 'Socialising', 'Studying', 'Wellbeing', 'Work', 'Housework'],
+        required: true
+    },
+    //checkboxes to select multiple
+    day: [{
+        type: String,
+        enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        required: true
+    }],
+    //time will need to increment from 30mins, 1hour, 1hour 30mins, 2hours etc
+    // possibly use enum  and have it increment? Then in EJS just print the hours so it's not 
+    // a user picking 600 minutes trying to figure out how many hours
+    time: {
+        type: Number,
+        min: 0,
+        max: 1400,
+        required: true
+    },
+    recordedGoal: {
+        type: Boolean
+    },
+    journals: [journalSchema],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Activity', activitySchema)
