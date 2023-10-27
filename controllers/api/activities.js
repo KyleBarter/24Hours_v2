6e107aba6c8df67925ffca1d973bca927d1badf3
KyleBarter <kylebarter4@gmail.com>
2023-10-27 15:34:55 +0100

    skeleton for route, controllers

--- controllers/api/activities.js
+++ controllers/api/activities.js
@@ -3,5 +3,34 @@ const User = require('../../models/user')
 
 
 module.exports = {
+    create,
+    index,
+    show,
+    update,
+    edit,
+    deleteActivity
+}
+
+async function create(req, res, next) {
     
+}
+
+async function index(req, res, next) {
+
+}
+
+async function show(req, res, next) {
+
+}
+
+async function update(req, res, next) {
+
+}
+
+async function edit(req, res, next) {
+
+}
+
+async function deleteActivity(req, res, next) {
+
 }
---
+++ routes/api/activities.js
@@ -0,0 +1,3 @@
+const express = require('express')
+const router = express.Router
+const activitiesCtrl = require('../../controllers/api/activities')
