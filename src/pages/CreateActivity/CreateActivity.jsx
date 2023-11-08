dc5da68e4312174e35d14714139d3480e879f865
KyleBarter <kylebarter4@gmail.com>
2023-11-08 19:20:24 +0000

    activity form created

--- src/pages/CreateActivity/CreateActivity.jsx
+++ src/pages/CreateActivity/CreateActivity.jsx
@@ -1,10 +1,80 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as activityAPI from "../../utilities/activities-api"
 
 export default function CreateActivity() {
+  const navigate = useNavigate()
 
+  const initialState = {
+    activity: '',
+    activityType: '',
+    day: [],
+    time: '',
+    recordedGoal: ''
+  }
+
+  const [activity, setActivity] = useState(initialState)
+
+  const handleChange = (evt) => {
+    const { activity, value } = evt.target;
+    setActivity({ ...activity, [activity]: value })
+  }
+
+  async function handleSubmit(evt){
+    evt.preventDefault()
+    try {
+      const res = await activityAPI.addActivity(activity)
+      navigate(`/activities/${res._id}`)
+    } catch (err) {
+      console.log('add activity submit error ->', err)
+    }
+  }
   
   return (
     <>
-      <h1>Create an activity</h1>
+      <div className="create-activity-form-parent">
+        <h1>Create an activity</h1>
+        <form className="create-activity-form">
+          <input 
+            type="text" 
+            name="activity"
+            placeholder="Activity"
+            value={activity.name}
+            onChange={handleChange}
+          />
+          <select name="activityType" value={activity.activityType}>
+            <option>-- Activity type --</option>
+            <option>Cooking</option>
+            <option>Exercise</option>
+            <option>Gardening</option>
+            <option>Hobby</option>
+            <option>Housework</option>
+            <option>Hygiene</option>
+            <option>Reading</option>
+            <option>Religious practice</option>
+            <option>Sleeping</option>
+            <option>Socialising</option>
+            <option>Studying</option>
+            <option>Wellbeing</option>
+          </select>
+          <input type="checkbox" name="day" value={"Monday"}/> Monday
+          <input type="checkbox" name="day" value={"Tuesday"}/> Tuesday
+          <input type="checkbox" name="day" value={"Wednesday"}/> Wednesday
+          <input type="checkbox" name="day" value={"Thursday"}/> Thursday
+          <input type="checkbox" name="day" value={"Friday"}/> Friday
+          <input type="checkbox" name="day" value={"Saturday"}/> Saturday
+          <input type="checkbox" name="day" value={"Sunday"}/> Sunday
+          <input 
+            type="number" 
+            name="time" 
+            value={activity.time} 
+            placeholder="Minutes a day"
+            onChange={handleChange}  
+          />
+          <input type="checkbox" name="recordedGoal" value={activity.recordedGoal}/> Recorded goal?
+          <input type="submit" onSubmit={handleSubmit}/>
+        </form>
+      </div>
     </>
 
   );
